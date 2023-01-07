2cf79ba8b066b1add7abbf8b6753dcdf850e577b
Emir <ekalajdzij1@etf.unsa.ba>
2023-01-07 14:59:35 +0100

    Main class update

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,14 +1,10 @@
 package ba.unsa.etf.rpr;
 
-import controls.JavaFXKlasa;
-import controls.PocetnaController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -18,7 +14,7 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class App extends Application{
     @Override
         public void start(Stage stage) throws IOException {
-            FXMLLoader fxmlLoader = new FXMLLoader(JavaFXKlasa.HelloApplication.class.getResource("/fxml/pocetna.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/pocetna.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
             stage.setTitle("");
             stage.setResizable(false);
