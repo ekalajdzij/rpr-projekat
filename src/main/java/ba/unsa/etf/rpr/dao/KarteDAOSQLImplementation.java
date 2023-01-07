3ef42a7f9457404b669158193cf9d381f36e2422
Emir <ekalajdzij1@etf.unsa.ba>
2023-01-07 13:38:29 +0100

    KarteDAOSQLImplementation update

--- src/main/java/ba/unsa/etf/rpr/dao/KarteDAOSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KarteDAOSQLImplementation.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class KarteDAOSQLImplementation extends AbstractDAO<Karte> implements KarteDAO {
-
+    private static KarteDAOSQLImplementation instance = null;
     public KarteDAOSQLImplementation() {super("Karte");}
     @Override
     public Karte row2object(ResultSet rs) throws KarteException {
