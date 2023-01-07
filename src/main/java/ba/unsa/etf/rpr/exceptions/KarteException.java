d57822b572604e28a09e9c3c6bff493f00e5690a
Emir <ekalajdzij1@etf.unsa.ba>
2023-01-07 13:25:16 +0100

    Code cleanup

--- src/main/java/ba/unsa/etf/rpr/exceptions/KarteException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/KarteException.java
@@ -2,8 +2,7 @@ package ba.unsa.etf.rpr.exceptions;
 
 public class KarteException extends Exception{
     public KarteException(String poruka, Exception razlog) {
-        super(poruka,razlog;
-    }
+        super(poruka,razlog);}
     public KarteException(String poruka) {
         super(poruka);
     }
