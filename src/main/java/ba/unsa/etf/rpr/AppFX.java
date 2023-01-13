541e477070cdb497525d0a1ee590ae16cb888834
Emir <ekalajdzij1@etf.unsa.ba>
2023-01-13 15:21:46 +0100

    Testiranje metoda

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,5 +1,12 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.bussines.KupacManager;
+import ba.unsa.etf.rpr.dao.KarteDAO;
+import ba.unsa.etf.rpr.dao.KarteDAOSQLImplementation;
+import ba.unsa.etf.rpr.dao.KupacDAO;
+import ba.unsa.etf.rpr.dao.KupacDAOSQLImplementation;
+import ba.unsa.etf.rpr.domain.Kupac;
+import ba.unsa.etf.rpr.exceptions.KarteException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,6 +14,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -22,11 +30,13 @@ public class AppFX extends Application{
             stage.show();
 
         }
-        public static void main(String[] args) throws SQLException {
+        public static void main(String[] args) throws KarteException {
             launch();
             //KupacDAO kDAO = new KupacDAOSQLImplementation();
-            //Kupac k = kDAO.getById(0);
+            //Kupac k = kDAO.getById(1);
             //System.out.println(k);
+            //int id = kDAO.getId("Karim Smith");
+            //System.out.println(id);
             //KarteDAO kartaD = new KarteDAOSQLImplementation();
             //List<Karte> karte = new ArrayList<>();
             //karte = kartaD.getAll();
@@ -42,7 +52,7 @@ public class AppFX extends Application{
             KarteDAO k = new KarteDAOSQLImplementation();
             List<String> l = k.getAllKarte();
             for (String x : l) System.out.println(x);
-            KarteDAO k = new KarteDAOSQLImplementation();
+           /* KarteDAO k = new KarteDAOSQLImplementation();
             int in = k.dajIdKarte("Film");
             Double cijena = k.dajCijenu(in);
             int pid = k.dajIdProdavcaKarte("Film");
