09ec330698d174a4dc515affe2e5641975fe973d
Emir <ekalajdzij1@etf.unsa.ba>
2023-01-18 18:55:28 +0100

    Dodan test

--- src/test/java/ba/unsa/etf/rpr/AppFXTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppFXTest.java
@@ -1,7 +1,18 @@
 package ba.unsa.etf.rpr;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ba.unsa.etf.rpr.bussines.KarteManager;
+import ba.unsa.etf.rpr.bussines.KupacManager;
+import ba.unsa.etf.rpr.bussines.ProdavacManager;
+import ba.unsa.etf.rpr.domain.Karte;
+import ba.unsa.etf.rpr.domain.Kupac;
+import ba.unsa.etf.rpr.domain.Prodavac;
+import ba.unsa.etf.rpr.exceptions.KarteException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,12 +20,23 @@ import org.junit.jupiter.api.Test;
  */
 public class AppFXTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test1() throws KarteException {
+        KupacManager kupacManager = new KupacManager();
+        Kupac kupac = kupacManager.getById(1);
+        Assertions.assertEquals(1,kupac.getId());
+
+        ProdavacManager prodavacManager = new ProdavacManager();
+        Prodavac prodavac = prodavacManager.getById(1);
+        Assertions.assertEquals(1,prodavac.getId());
+
+        KarteManager karteManager = new KarteManager();
+        Karte karta = karteManager.getById(1);
+        Assertions.assertEquals(1,karta.getId());
     }
+
+
+
+
 }
