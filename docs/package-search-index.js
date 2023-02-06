81c2ccfa8dd9bba1b6ab807fd7f3d24874c8d4ef
Emir <ekalajdzij1@etf.unsa.ba>
2023-02-06 17:20:25 +0100

    Generated javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ba.unsa.etf.rpr"},{"l":"ba.unsa.etf.rpr.bussines"},{"l":"ba.unsa.etf.rpr.dao"},{"l":"ba.unsa.etf.rpr.domain"},{"l":"ba.unsa.etf.rpr.exceptions"},{"l":"controls"}];updateSearchResults();
