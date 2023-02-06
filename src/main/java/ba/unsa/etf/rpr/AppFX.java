cb7bc8c542c7194b48d711e7a589bf4c68da35c7
Emir <ekalajdzij1@etf.unsa.ba>
2023-02-06 16:59:54 +0100

    Code cleanup

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -18,6 +18,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
@@ -36,36 +37,5 @@ public class AppFX extends Application{
         }
         public static void main(String[] args) throws KarteException {
             launch();
-            //KupacDAO kDAO = new KupacDAOSQLImplementation();
-            //Kupac k = kDAO.getById(1);
-            //System.out.println(k);
-            //int id = kDAO.getId("Karim Smith");
-            //System.out.println(id);
-            //KarteDAO kartaD = new KarteDAOSQLImplementation();
-            //List<Karte> karte = new ArrayList<>();
-            //karte = kartaD.getAll();
-            //for (Karte x : karte) System.out.println(x);
-            /*ProdavacDAO pDAO = new ProdavacDAOSQLImplementation();
-            int id = pDAO.getId("Damir Hos");
-            Prodavac p = pDAO.getById(id);
-            System.out.println(p);
-            KupacDAO kDAO = new KupacDAOSQLImplementation();
-            int idd = kDAO.getId("Karim Smith");
-            Kupac k = kDAO.getById(idd);
-            System.out.println(k);
-
-            KarteDAO k = new KarteDAOSQLImplementation();
-            List<String> l = k.getAllKarte();
-            for (String x : l) System.out.println(x);
-           /* KarteDAO k = new KarteDAOSQLImplementation();
-            int in = k.dajIdKarte("Film");
-            Double cijena = k.dajCijenu(in);
-            int pid = k.dajIdProdavcaKarte("Film");
-            System.out.println(cijena);*/
-            /*KarteManager m = new KarteManager();
-            Karte karta = m.getById(6);
-            System.out.println(karta);*/
-
-
         }
 }
