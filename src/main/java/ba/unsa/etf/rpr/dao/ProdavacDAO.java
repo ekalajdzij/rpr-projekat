aa3ea11ceff59b8886a2d626e15c11a502094e76
Emir <ekalajdzij1@etf.unsa.ba>
2023-02-06 16:59:12 +0100

    Dodani javadoc comments

--- src/main/java/ba/unsa/etf/rpr/dao/ProdavacDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ProdavacDAO.java
@@ -3,7 +3,18 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Prodavac;
 import ba.unsa.etf.rpr.exceptions.KarteException;
 
+/**
+ * The interface of Prodavac dao
+ */
+
 public interface ProdavacDAO extends DAO<Prodavac> {
+    /**
+     * Gets id of a seller
+     *
+     * @param ime - the name of the seller
+     * @return id of the seller
+     * @throws KarteException if an error occurs
+     */
     int getId(String ime) throws KarteException;
 
 }
