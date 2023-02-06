7ce87213303ec6e3f259d57b997c72876fa5545f
Emir <ekalajdzij1@etf.unsa.ba>
2023-02-06 16:58:50 +0100

    Dodani javadoc comments

--- src/main/java/ba/unsa/etf/rpr/dao/KupacDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KupacDAO.java
@@ -3,6 +3,17 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Kupac;
 import ba.unsa.etf.rpr.exceptions.KarteException;
 
+/**
+ * The interface of Kupac dao.
+ */
+
 public interface KupacDAO extends DAO<Kupac> {
+    /**
+     * Gets id of a buyer
+     *
+     * @param ime - the name of the buyer
+     * @return id of the buyer
+     * @throws KarteException if an error occurs
+     */
     int getId(String ime) throws KarteException;
 }
