2e01f0dadf7b76a6488cbcd8c4ed24153d594e1b
Emir <ekalajdzij1@etf.unsa.ba>
2023-02-08 17:37:19 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/controls/KupioController.java
+++ src/main/java/ba/unsa/etf/rpr/controls/KupioController.java
@@ -4,9 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class KupioController {
--- src/main/java/ba/unsa/etf/rpr/controls/PocetnaController.java
+++ src/main/java/ba/unsa/etf/rpr/controls/PocetnaController.java
@@ -9,8 +9,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
